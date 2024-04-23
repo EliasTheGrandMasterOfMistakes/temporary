685c35d13413069ad9dbc72c5bc707f9b8f63176
eliasofwaffle <eliascontato@protonmail.com>
2024-04-23 20:09:38 +0000

    fix

--- build_rom.sh
+++ build_rom.sh
@@ -8,8 +8,12 @@ source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
+xport BUILD_USER=EliasOfWaffle
+export BUILD_USERNAME=EliasOfWaffle
+export KBUILD_BUILD_USER=EliasOfWaffle
+export TZ=America/Sao_Paulo
 brunch a10 eng
-export TZ=America/Sao_Paulo #put before last build command
+#export TZ=America/Sao_Paulo #put before last build command
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
